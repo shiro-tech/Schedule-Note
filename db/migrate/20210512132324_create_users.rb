ba411a348b00659895a60957f50413aae9e79f6d
シロー <kishi.rabbit313@gmail.com>
2021-05-30 00:30:20 +0900

    meisyou henkou

--- db/migrate/20210512132324_create_users.rb
+++ db/migrate/20210512132324_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :name, null: false
       t.string :email, null: false
       t.string :password, null: false
-      t.string :password_definication, null: false
+      t.string :password_confirmation, null: false
       t.string :company
       t.string :cellphone
       t.string :admin
